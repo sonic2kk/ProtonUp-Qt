10ac4345c992dd495f8485dddecd5c29032d6f03
Eamonn Rea <eamonnrea@gmail.com>
2024-07-09 04:02:06 +0100

    ctmods: Remove unused Wine-GE imports

--- pupgui2/resources/ctmods/ctmod_00winege.py
+++ pupgui2/resources/ctmods/ctmod_00winege.py
@@ -2,15 +2,8 @@
 # Wine-GE
 # Copyright (C) 2021 DavidoTek, partially based on AUNaseef's protonup
 
-import os
-import requests
-import hashlib
+from PySide6.QtCore import QCoreApplication
 
-from PySide6.QtCore import QObject, QCoreApplication, Signal, Property
-
-from pupgui2.datastructures import Launcher
-from pupgui2.util import ghapi_rlcheck, extract_tar, get_launcher_from_installdir
-from pupgui2.util import build_headers_with_authorization
 from pupgui2.resources.ctmods.ctmod_00protonge import CtInstaller as GEProtonInstaller
 
 

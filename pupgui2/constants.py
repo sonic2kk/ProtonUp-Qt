8d7b112b242f30d4347761ea0affe36cdb4daa31
Eamonn Rea <eamonnrea@gmail.com>
2024-08-02 00:21:39 +0100

    constants: Improve type hints
    * Add type hints for data structures
    * Add explicit type hints for variables that rely on
      function return values, to note what we expect them
      to hold
    * Add type hint return to PALETTE_DARK

--- pupgui2/constants.py
+++ pupgui2/constants.py
@@ -8,28 +8,28 @@ from PySide6.QtGui import QColor, QPalette
 APP_NAME = 'ProtonUp-Qt'
 APP_VERSION = '2.10.0'
 APP_ID = 'net.davidotek.pupgui2'
-APP_THEMES = ( 'light', 'dark', 'system', None )
-APP_ICON_FILE = os.path.join(xdg_config_home, 'pupgui/appicon256.png')
+APP_THEMES: tuple[str, str, str, None] = ( 'light', 'dark', 'system', None )
+APP_ICON_FILE: str = os.path.join(xdg_config_home, 'pupgui/appicon256.png')
 APP_GHAPI_URL = 'https://api.github.com/repos/Davidotek/ProtonUp-qt/releases'
 DAVIDOTEK_KOFI_URL = 'https://ko-fi.com/davidotek'
 PROTONUPQT_GITHUB_URL = 'https://github.com/DavidoTek/ProtonUp-Qt'
-ABOUT_TEXT = '''\
+ABOUT_TEXT: str = '''\
 {APP_NAME} v{APP_VERSION} by DavidoTek: <a href="{PROTONUPQT_GITHUB_URL}">https://github.com/DavidoTek/ProtonUp-Qt</a><br />
 Copyright (C) 2021-2024 DavidoTek, licensed under GPLv3
 '''.format(APP_NAME=APP_NAME, APP_VERSION=APP_VERSION, PROTONUPQT_GITHUB_URL=PROTONUPQT_GITHUB_URL)
 BUILD_INFO = 'built from source'
 
-CONFIG_FILE = os.path.join(xdg_config_home, 'pupgui/config.ini')
-TEMP_DIR = os.path.join(os.getenv('XDG_CACHE_HOME'), 'tmp', 'pupgui2.a70200/') if os.path.exists(os.getenv('XDG_CACHE_HOME', '')) else '/tmp/pupgui2.a70200/'
-HOME_DIR = os.path.expanduser('~')
+CONFIG_FILE: str = os.path.join(xdg_config_home, 'pupgui/config.ini')
+TEMP_DIR: str = os.path.join(os.getenv('XDG_CACHE_HOME', ''), 'tmp', 'pupgui2.a70200/') if os.path.exists(os.getenv('XDG_CACHE_HOME', '')) else '/tmp/pupgui2.a70200/'
+HOME_DIR: str = os.path.expanduser('~')
 
 IS_FLATPAK: bool = os.path.exists('/.flatpak-info')
 
 # DBus constants
-DBUS_APPLICATION_URI = f'application://{APP_ID}.desktop'
+DBUS_APPLICATION_URI: str = f'application://{APP_ID}.desktop'
 DBUS_DOWNLOAD_OBJECT_BASEPATH = '/net/davidotek/pupgui2'
 
-DBUS_INTERFACES_AND_SIGNALS = {
+DBUS_INTERFACES_AND_SIGNALS: dict[str, dict[str, str]] = {
     'LauncherEntryUpdate': {
         'interface': 'com.canonical.Unity.LauncherEntry',
         'signal': 'Update',
@@ -38,7 +38,7 @@ DBUS_INTERFACES_AND_SIGNALS = {
 
 # support different Steam root directories, building paths relative to HOME_DIR (i.e. /home/gaben/.local/share/Steam)
 # Use os.path.realpath to expand all _STEAM_ROOT paths
-_POSSIBLE_STEAM_ROOTS = [
+_POSSIBLE_STEAM_ROOTS: list[str] = [
     os.path.realpath(os.path.join(HOME_DIR, _STEAM_ROOT)) for _STEAM_ROOT in ['.local/share/Steam', '.steam/root', '.steam/steam', '.steam/debian-installation']
 ]
 
@@ -49,7 +49,7 @@ _POSSIBLE_STEAM_ROOTS = list(dict.fromkeys(_POSSIBLE_STEAM_ROOTS))
 # i.e. '~/.steam/root' is usually a symlink to '~/.local/share/Steam'
 # These paths may still not be valid installations however, as they could be leftother paths from an old Steam installation without the data files we need ('config.vdf' and 'libraryfolders.vdf')
 # We catch this later on in util#is_valid_launcher_installation though
-POSSIBLE_INSTALL_LOCATIONS = [
+POSSIBLE_INSTALL_LOCATIONS: list[dict[str, str]] = [
         {
             'install_dir': f'{_STEAM_ROOT}/compatibilitytools.d/',
             'display_name': 'Steam',
@@ -76,11 +76,11 @@ POSSIBLE_INSTALL_LOCATIONS += [
     {'install_dir': '~/.var/app/io.github.fastrizwaan.WineZGUI/data/winezgui/Runners/', 'display_name': 'WineZGUI Flatpak', 'launcher': 'winezgui', 'type': 'flatpak', 'icon': 'io.github.fastrizwaan.WineZGUI'}
 ]
 
-def PALETTE_DARK():
+def PALETTE_DARK() -> QPalette:
     """ returns dark color palette """
-    palette_base = QColor(12, 12, 12)
+    palette_base: QColor = QColor(12, 12, 12)
 
-    palette_dark = QPalette()
+    palette_dark: QPalette = QPalette()
     palette_dark.setColor(QPalette.Window, QColor(30, 30, 30))
     palette_dark.setColor(QPalette.WindowText, Qt.white)
     palette_dark.setColor(QPalette.Base, palette_base)
@@ -96,36 +96,36 @@ def PALETTE_DARK():
     palette_dark.setColor(QPalette.HighlightedText, Qt.black)
     return palette_dark
 
-PROTONDB_COLORS = {'platinum': '#b4c7dc', 'gold': '#cfb53b', 'silver': '#a6a6a6', 'bronze': '#cd7f32', 'borked': '#ff0000', 'pending': '#748472' }
+PROTONDB_COLORS: dict[str, str] = {'platinum': '#b4c7dc', 'gold': '#cfb53b', 'silver': '#a6a6a6', 'bronze': '#cd7f32', 'borked': '#ff0000', 'pending': '#748472' }
 
 STEAM_API_GETAPPLIST_URL = 'https://api.steampowered.com/ISteamApps/GetAppList/v2/'
 STEAM_APP_PAGE_URL = 'https://store.steampowered.com/app/'
 AWACY_GAME_LIST_URL = 'https://raw.githubusercontent.com/Starz0r/AreWeAntiCheatYet/master/games.json'
 AWACY_WEB_URL = 'https://areweanticheatyet.com/?search={GAMENAME}&sortOrder=&sortBy='
-LOCAL_AWACY_GAME_LIST = os.path.join(TEMP_DIR, 'awacy_games.json')
+LOCAL_AWACY_GAME_LIST: str = os.path.join(TEMP_DIR, 'awacy_games.json')
 PROTONDB_API_URL = 'https://www.protondb.com/api/v1/reports/summaries/{game_id}.json'
 PROTONDB_APP_PAGE_URL = 'https://protondb.com/app/'
 
 STEAM_BOXTRON_FLATPAK_APPSTREAM = 'appstream://com.valvesoftware.Steam.CompatibilityTool.Boxtron'
 STEAM_STL_FLATPAK_APPSTREAM = 'appstream://com.valvesoftware.Steam.Utility.steamtinkerlaunch'
 
-STEAM_STL_INSTALL_PATH = os.path.join(HOME_DIR, 'stl')
-STEAM_STL_CONFIG_PATH = os.path.join(HOME_DIR, '.config', 'steamtinkerlaunch')
-STEAM_STL_CACHE_PATH = os.path.join(HOME_DIR, '.cache', 'steamtinkerlaunch')
-STEAM_STL_DATA_PATH = os.path.join(HOME_DIR, '.local', 'share', 'steamtinkerlaunch')
-STEAM_STL_SHELL_FILES = [ '.bashrc', '.zshrc', '.kshrc' ]
-STEAM_STL_FISH_VARIABLES = os.path.join(HOME_DIR, '.config/fish/fish_variables')
+STEAM_STL_INSTALL_PATH: str = os.path.join(HOME_DIR, 'stl')
+STEAM_STL_CONFIG_PATH: str = os.path.join(HOME_DIR, '.config', 'steamtinkerlaunch')
+STEAM_STL_CACHE_PATH: str = os.path.join(HOME_DIR, '.cache', 'steamtinkerlaunch')
+STEAM_STL_DATA_PATH: str = os.path.join(HOME_DIR, '.local', 'share', 'steamtinkerlaunch')
+STEAM_STL_SHELL_FILES: list[str] = [ '.bashrc', '.zshrc', '.kshrc' ]
+STEAM_STL_FISH_VARIABLES: str = os.path.join(HOME_DIR, '.config/fish/fish_variables')
 
 LUTRIS_WEB_URL = 'https://lutris.net/games/'
 EPIC_STORE_URL = 'https://store.epicgames.com/p/'
 
 GITHUB_API = 'https://api.github.com/'
 # GitLab can have any self-hosted instance, so we store a list of known GitLab instances
-GITLAB_API = [
+GITLAB_API: list[str] = [
     'https://gitlab.com/api/'
 ]
 
-GITLAB_API_RATELIMIT_TEXT = [
+GITLAB_API_RATELIMIT_TEXT: list[str] = [
     'Rate limit exceeded; see https://docs.gitlab.com/ee/user/gitlab_com/#gitlabcom-specific-rate-limits for more details',
     'Rate limit exceeded',
     'Retry later'
